16834a535d211612c618cb43467eaed1b42e54e7
Rohitjangid17 <jangidrohit817@gmail.com>
2024-08-15 17:17:51 +0530

    add cors origin

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const productRoutes = require("./routes/product.route");
 const app = express();
 const port = 3000;
@@ -9,12 +10,17 @@ app.get("/", (req, res) => {
     res.json({ message: "Rohit jangid fullstack developer." });
 });
 
+app.use(cors());
+
 // Routes call
 app.use("/api/products", productRoutes);
 
 // database connection
 const connectDb = async () => {
-    await mongoose.connect("mongodb+srv://fdeveloperrohitjangid:RUkcWQAkmtx4IZ7h@fastkartnew.ywhjl.mongodb.net/");
+    // await mongoose.connect("mongodb+srv://fdeveloperrohitjangid:RUkcWQAkmtx4IZ7h@fastkartnew.ywhjl.mongodb.net/");
+    await mongoose.connect("mongodb://localhost:27017", {
+        dbName: "fastkartDb"
+    });
     console.log("mongodb connected");
 }
 
