81e67b86e2f90799cfc9b73e35333bc8c52eee8b
Rohitjangid17 <jangidrohit817@gmail.com>
2024-08-17 15:52:54 +0530

    category post and get category list api done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 const productRoutes = require("./routes/product.route");
+const categoryRoutes = require("./routes/category.route");
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -14,13 +15,14 @@ app.use(cors());
 
 // Routes call
 app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 
 // database connection
 const connectDb = async () => {
-    await mongoose.connect("mongodb+srv://fdeveloperrohitjangid:RUkcWQAkmtx4IZ7h@fastkartnew.ywhjl.mongodb.net/");
-    // await mongoose.connect("mongodb://localhost:27017", {
-    //     dbName: "fastkartDb"
-    // });
+    // await mongoose.connect("mongodb+srv://fdeveloperrohitjangid:RUkcWQAkmtx4IZ7h@fastkartnew.ywhjl.mongodb.net/");
+    await mongoose.connect("mongodb://localhost:27017", {
+        dbName: "fastkartDb"
+    });
     console.log("mongodb connected");
 }
 
