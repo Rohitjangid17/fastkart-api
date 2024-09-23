d4d4ef65eb0bd3b0c6fe45d80b950ab86e798d60
Rohitjangid17 <jangidrohit817@gmail.com>
2024-09-23 22:16:19 +0530

    manipulate return object

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -24,19 +24,23 @@ const signInStore = async (req, res) => {
     const { mobileNumber, password } = req.body;
 
     try {
-        const store = await Auth.findOne({ mobileNumber }); // Use mobileNumber to find the store
+        const store = await Auth.findOne({ mobileNumber });
         if (!store) {
             return res.status(404).json({ message: "Store not found" });
         }
 
-        // Assuming the password is hashed, compare the hashed password
         const isMatch = await bcrypt.compare(password, store.password);
         if (!isMatch) {
             return res.status(400).json({ message: "Invalid credentials" });
         }
 
+        // Generate token
         const token = jwt.sign({ id: store._id }, "Ctpl#2023?");
-        res.status(200).json({ ...req.body, token });
+
+        // Exclude the password from the store object
+        const { password: hashedPassword, ...storeDetails } = store.toObject(); // Rename password to hashedPassword
+
+        res.status(200).json({ ...storeDetails, token });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
